35031081b86692c4e6d7d70f8841282611cfadf0
kkebuka <kkebuka@gmail.com>
2024-06-15 12:07:47 +0100

    update to reducer js

--- src/reducer.js
+++ src/reducer.js
@@ -3,9 +3,10 @@ export const initialState = {
   playlists: [],
   playing: false,
   item: null,
+  discover_weekly: null,
   //Remove after finsish developings
-  token:
-    "BQAmKHdBM4Wawa6ZWUWM-dR_WZo1ZvUXEjVuUYtFumCeDzb-80Yr5277p9GSRtdeggLQANRsnvZ8Zu9T0WWDr1IpyzkBBhGOfMmC0exu4h7_dIcwudFKt18UVv4wKDPt_vq67QHYkjf3aocTaUVzA-eIDWJcv9jlsXCUdVYhOl9Ir2ZvzXOZOQbD7X0XCVDjkjytj7NVh25wCinLr6WR",
+  //   token:
+  // "BQAmKHdBM4Wawa6ZWUWM-dR_WZo1ZvUXEjVuUYtFumCeDzb-80Yr5277p9GSRtdeggLQANRsnvZ8Zu9T0WWDr1IpyzkBBhGOfMmC0exu4h7_dIcwudFKt18UVv4wKDPt_vq67QHYkjf3aocTaUVzA-eIDWJcv9jlsXCUdVYhOl9Ir2ZvzXOZOQbD7X0XCVDjkjytj7NVh25wCinLr6WR",
 };
 
 const reducer = (state, action) => {
@@ -26,6 +27,12 @@ const reducer = (state, action) => {
       return {
         ...state,
         playlists: action.playlists,
+      };
+
+    case "SET_DISCOVER_WEEKLY":
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
       };
     default:
       return state;
